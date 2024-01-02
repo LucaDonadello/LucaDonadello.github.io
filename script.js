2a5b03d872997873dc8786d93fbe58dd8209be4b
LucaDonadello <luca.donadello99@gmail.com>
2024-01-02 11:50:32 -0600

    publish
    add everything to Github

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    //target element and toggle
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
